812edde4c204e9afbdb9ee3ecb57a4df711ac3a3
표호근 <hkpyo@thenet-global.com>
2018-04-12 15:58:09 +0900

    갤러리 > 선택리스트 삭제 기능추가, 선택시 중복체크기능 추가

--- (continue)gallery/script.js
+++ (continue)gallery/script.js
@@ -2,9 +2,9 @@
 function selectImg(event) {
 	var btn = event.target.previousSibling;
 	var btnSrc = btn.src;
-	console.log("이미지 실제 주소 : " + btnSrc);
-	selectDo(btnSrc);
-	listUp(btnSrc);
+	// console.log("이미지 실제 주소 : " + btnSrc);
+	check(btnSrc);
+	
 }
 
 // fn 배열에 추가
@@ -16,13 +16,35 @@ function selectDo(btnSrc){
 	selected.push(btnSrc);
 }
 
+function check(btnSrc) {
+	console.log("중복체크",selected.indexOf(btnSrc));
+	if ( selected.indexOf(btnSrc) === -1 ) {
+		selectDo(btnSrc);
+		listUp(btnSrc);
+	} else {
+		console.log("이미 선택되었습니다.")
+	}
+
+}
+
+// 중복검사
+
 // fn 리스트에 넣기
 function listUp(btnSrc){
 	
-	var addLi = document.createElement("LI");
-	var addImg = document.createElement("IMG");
+	var addLi = document.createElement("li");
+	// 이미지 태그 생성
+	var addImg = new Image();
 	addImg.src = btnSrc;
+	addImg.addEventListener('load',function(){
+
+	});
+	
+	addButton = document.createElement('button');
+	addButton.setAttribute('onclick','delList(this);');
+	addButton.innerHTML = '삭제';
 	addLi.appendChild(addImg);
+	addLi.appendChild(addButton);
 
 	var listBasket = document.getElementsByClassName("listUp");
 	listBasket[0].appendChild(addLi);
@@ -30,6 +52,17 @@ function listUp(btnSrc){
 }
 
 // fn 삭제
+function delList(delTarget){
+	console.log("선택한 버튼의 리스트",delTarget.parentElement);
+	var delTargetList = delTarget.parentElement;
+	delTargetList.parentNode.removeChild(delTargetList);
+	delSeleted(delTarget);
+}
 
 // fn 배열에 삭제
+function delSeleted(delTarget) {
+	var delBtn = delTarget.previousSibling.src;
+	selected.pop(delBtn);
+}
+
 

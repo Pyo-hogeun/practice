8d6bad0184ac48e76d01358ac95c4ed8b7337419
표호근 <hkpyo@thenet-global.com>
2018-05-03 20:52:23 +0900

    function 추가

--- (continue)validate/script.js
+++ (continue)validate/script.js
@@ -1,5 +1,16 @@
-var validate;
-validate = function validate(){
+var validate = {
+	validate: function (){
 	var inputs = document.getElementsByTagName("input");
-
+		for (var i = 0; i < inputs.length; i++) {
+			var a = inputs[i].value;
+		}
+		if( a == "" ){
+			validate.save();
+		} else {
+			console.log("통과");
+		}
+	},
+	save: function(){
+		alert("not yet");
+	}
 }

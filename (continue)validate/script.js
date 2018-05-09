1cf1cd82b545c27aa6d7e3148d7ccd219c905472
표호근 <hkpyo@thenet-global.com>
2018-05-09 23:22:14 +0900

    전화번호 국번 유효성 함수 추가

--- (continue)validate/script.js
+++ (continue)validate/script.js
@@ -7,7 +7,7 @@ var validate = {
 		if( a == "" ){ // input 이 비어있다면 empty 알림
 			validate.empty();
 		} else {
-			console.log("통과");
+			console.log("빈칸없음 통과");
 			// 휴대전화번호 검사
 			validate.celphoneNumber();
 		}
@@ -18,15 +18,16 @@ var validate = {
 	//휴대전화번호
 	celphoneNumber: function(){
 		var celphoneNumber = document.getElementsByName('celphonenumber');
-		// ㄴ첫번째
+		// 첫번째
 		// 국번 010 ~ 019 까지
 		// 총 세자리
 		var cel01 = celphoneNumber[0].value;
-		var patt1 =  /[01][0-9]/g;
-		if( cel01 == cel01.match(patt1) ){
-			console.log('celphonenumber[0] pass')
+		var patt1 =  /01[0-9]/g;
+		var matchValue = cel01.match(patt1)
+		if( matchValue == null ){
+			console.log('휴대전화번호 국번이 잘못됐습니다.')
 		} else {
-			celphoneNumber[0].addEventListener('onfocusout', validate.alertPhrase());
+			console.log('휴대전화번호 국번 통과')
 		}
 	},
 	alertPhrase: function(){

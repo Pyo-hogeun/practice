f3f27ba35ad1a667594776efd9dfa43a657b9d6a
표호근 <pyohogeun@friver.co.kr>
2018-07-12 17:26:09 +0900

    sitemap 연습

--- (new)findKim-javascript_indexOf/script.js
+++ (new)findKim-javascript_indexOf/script.js
@@ -1,9 +1,15 @@
-function solution(seoul) {
+var fn = function solution(seoul) {
     var answer = '';
     answer = seoul.indexOf("Kim");
     console.log("김서방은 " + answer +"에 있다.");
     return "김서방은 " + answer +"에 있다.";
+
+    hello: function(){ alert( fn.hello() ); }
+
+
 }
 
 var seoul = ["Jane", "Kim", "pyo", "You", "kyu"]
 solution(seoul);
+
+

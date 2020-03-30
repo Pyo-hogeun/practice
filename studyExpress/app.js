8e4e44388b8ebbb278006afefa82029191515eb3
pyohogeun <pyohogeun@friver.co.kr>
2020-03-30 17:51:31 +0900

    intro 편집후 redirect 구현 성공!

--- studyExpress/app.js
+++ studyExpress/app.js
@@ -13,10 +13,11 @@ app.get("/", (req, res) => {
     console.log("홈으로");
     res.render("home",{});
 });
-app.get("/intro", (req, res) => {
+app.get("/intro", (req, res, next) => {
     console.log("소개페이지로");
-    fs.readFile("/assets/intro-text.txt")
-    res.render("intro",{});
+    // fs.readFile("/assets/intro-text.txt");
+    res.render("intro",{})
+    // next();
 });
 app.get("/gallery", (req, res) => {
     console.log("갤러리로");
@@ -26,13 +27,19 @@ app.post("/postEditIntro", (req, res) => {
     console.log(req.body.content);
     fs.writeFile('assets/intro-text.txt', req.body.content , (err) => {
         if( err ) throw err;
-        console.log("저장완료");
-
+        
     });
-    res.redirect(req.get('referer'));
+    console.log("저장완료");
+    var status = {
+        "status": 200,
+        "message": 'login success',
+        "redirect": "/intro"
+    }
+    res.end(JSON.stringify(status));
+    //ajax 구현시에 페이지 이동하고 싶으면 수동으로 이렇게 하자 redirect는 안먹힘
     
     // res.json({ok: true});
-})
+});
 
 app.listen(8080, () => {
     console.log("서버가동중");
